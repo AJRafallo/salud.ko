6042f3146f05a173e8de684de3044401f761451f
Trisha <tsabanal@gbox.adnu.edu.ph>
2024-12-08 20:37:27 +0800

    changes

--- lib/screens/UserSide/medical_files_page.dart
+++ lib/screens/UserSide/medical_files_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:saludko/screens/widget/UploadRecords.dart';
-// not connected yet
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -49,7 +48,7 @@ class _MedicalFilesPageState extends State<MedicalFilesPage> {
     }
   }
 
-  // Fetch and sort folders dynamically from Firestore
+  // Fetch and sort folders from Firestore
   Stream<List<Map<String, dynamic>>> _getUserFolders(String userId) {
     return FirebaseFirestore.instance
         .collection('users')
@@ -220,11 +219,10 @@ class _MedicalFilesPageState extends State<MedicalFilesPage> {
                           return _buildFolder(
                             context: context,
                             label: folder["name"],
-                            isDeletable: folder["isDefault"] ?? false == false,
                             onTap: () => _navigateToFolder(
                               context,
                               folder["name"],
-                              folder["isDefault"] ?? false,
+                              !(folder["isDefault"] ?? false),
                               folder["id"],
                             ),
                           );
@@ -251,13 +249,13 @@ class _MedicalFilesPageState extends State<MedicalFilesPage> {
 
   // Navigate to folder content
   void _navigateToFolder(BuildContext context, String folderName,
-      bool isDefault, String folderId) {
+      bool isDeletable, String folderId) {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => FolderContentPage(
           folderName: folderName,
-          isDeletable: !isDefault,
+          isDeletable: isDeletable,
           folderId: folderId,
         ),
       ),
@@ -269,7 +267,6 @@ class _MedicalFilesPageState extends State<MedicalFilesPage> {
     required BuildContext context,
     required String label,
     required VoidCallback onTap,
-    bool isDeletable = false,
     IconData icon = Icons.folder,
     bool isCreateFolder = false,
   }) {
@@ -308,7 +305,7 @@ class _MedicalFilesPageState extends State<MedicalFilesPage> {
   }
 }
 
-// Folder Content Page with Delete Button for Custom Folders
+// Folder Content Page
 class FolderContentPage extends StatelessWidget {
   final String folderName;
   final bool isDeletable;
@@ -324,45 +321,296 @@ class FolderContentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(folderName)),
-      body: Center(
-        child: isDeletable
-            ? ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text("Delete Folder"),
-                        content: const Text(
-                            "Are you sure you want to delete this folder?"),
-                        actions: [
-                          TextButton(
-                            onPressed: () => Navigator.pop(context),
-                            child: const Text("Cancel"),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              FirebaseFirestore.instance
-                                  .collection('users')
-                                  .doc(FirebaseAuth.instance.currentUser!.uid)
-                                  .collection('folders')
-                                  .doc(folderId)
-                                  .delete();
-                              Navigator.pop(context);
-                              Navigator.pop(context);
-                            },
-                            child: const Text("Delete"),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: const Text("Delete Folder"),
-              )
-            : const Text("This folder cannot be deleted."),
+      appBar: AppBar(
+        title: Text(folderName),
+        actions: [
+          if (isDeletable) // Only show the menu for created folders
+            PopupMenuButton<String>(
+              onSelected: (value) {
+                if (value == 'edit') {
+                  _showEditFolderDialog(context);
+                } else if (value == 'delete') {
+                  _showDeleteFolderDialog(context);
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                return [
+                  const PopupMenuItem(
+                    value: 'edit',
+                    child: Text('Edit'),
+                  ),
+                  const PopupMenuItem(
+                    value: 'delete',
+                    child: Text('Delete'),
+                  ),
+                ];
+              },
+            ),
+        ],
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .collection('folders')
+            .doc(folderId)
+            .collection('files') // Assuming files are stored in a subcollection
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+
+          final files = snapshot.data?.docs ?? [];
+
+          if (files.isEmpty) {
+            return const Center(
+              child: Text(
+                "This folder is empty.",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              ),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: files.length,
+            itemBuilder: (context, index) {
+              final file = files[index];
+              return ListTile(
+                title: Text(file['name'] ?? 'Unnamed File'),
+                subtitle: Text(file['uploadedAt'].toDate().toString()),
+                leading: const Icon(Icons.file_present),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void _showDeleteFolderDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          title: const Center(
+            child: Text(
+              "Delete Folder",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 5),
+              const Text(
+                "Are you sure you want to delete this folder?",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14, color: Colors.black),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "You can still access your files at the \"All Files\" folder.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 11,
+                  color: Colors.black54,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => Navigator.pop(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    side: const BorderSide(
+                      color: Color(0xFF1A62B7),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    "Cancel",
+                    style: TextStyle(
+                      color: Color(0xFF1A62B7),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    await FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(FirebaseAuth.instance.currentUser!.uid)
+                        .collection('folders')
+                        .doc(folderId)
+                        .delete();
+
+                    Navigator.pop(context); // Close the dialog
+                    Navigator.pop(context); // Return to the previous screen
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFDB0000),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: 10,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    "Delete",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showEditFolderDialog(BuildContext context) {
+    final TextEditingController folderNameController =
+        TextEditingController(text: folderName);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          title: const Center(
+            child: Text(
+              "Edit Folder",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFD9D9D9),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: TextField(
+                  controller: folderNameController,
+                  decoration: const InputDecoration(
+                    hintText: "Folder Name",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actionsPadding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: 10,
+            bottom: 15,
+          ),
+          actions: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(
+                        color: Color(0xFF1A62B7),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      "Cancel",
+                      style: TextStyle(
+                        color: Color(0xFF1A62B7),
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final newName = folderNameController.text.trim();
+                      if (newName.isNotEmpty) {
+                        await FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(FirebaseAuth.instance.currentUser!.uid)
+                            .collection('folders')
+                            .doc(folderId)
+                            .update({"name": newName});
+
+                        Navigator.pop(context);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF1A62B7),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 10,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      "Save",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
